745ee17cddaff92df13e48a708e01556eeb0bc21
san0kq <sanrom94@mail.ru>
2023-01-10 23:33:58 +0300

    program with simple types methods

---
+++ Lesson_3/simple_types.py
@@ -0,0 +1,22 @@
+number_1 = 89
+print(type(number_1))
+
+number_2 = 87.223
+print(type(number_2))
+
+string_1 = 'Hello World'
+print(type(string_1))
+
+bool_1 = True
+print(type(bool_1))
+
+print(number_2 + number_1)
+print(number_2 ** number_1)
+print(round(number_2, 1))
+print(string_1.lower())
+print(string_1[::-1])
+print(string_1.find('o'))
+print(string_1 + '!')
+
+bool_2 = bool('False')
+print(bool_2)
