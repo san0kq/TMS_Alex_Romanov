96377163fd08a3705201dd991740e3d27c0cac89
san0kq <sanrom94@mail.ru>
2023-01-14 17:03:36 +0300

    task 6 is completed

---
+++ Lesson_4/task_6.py
@@ -0,0 +1,10 @@
+# Initialize two integers
+number_1 = int(input('Введите первое целое число: '))
+number_2 = int(input('Введите второе целое число: '))
+
+if number_1 > 10 and number_2 > 10:
+    print('Оба числа больше 10')
+elif number_1 > 10 or number_2 > 10:
+    print('Одно из чисел больше 10')
+elif not number_1 == bool(0):  # if number_1 not False
+    print('Условие с помощью преобразования типов')
