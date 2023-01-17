199087bf18667b9ce3b3293be86e0c05177b27fe
san0kq <sanrom94@mail.ru>
2023-01-17 23:15:49 +0300

    add while loop

--- Lesson_5/task_2.py
+++ Lesson_5/task_2.py
@@ -1,17 +1,21 @@
-name = input('Введите ваше имя: ')
-age = input('Введите ваш возраст: ')
+while True:
+    name = input('Введите ваше имя: ')
+    age = input('Введите ваш возраст (введите STOP, чтобы остановить программу): ')
 
-try:
-    age = int(age)
-    if age <= 0:
+    if age == 'STOP':  # Stopping code execution
+        break
+
+    try:
+        age = int(age)
+        if age <= 0:
+            print('Ошибка, повторите ввод')
+        elif age < 10:
+            print(f'Привет, шкет {name}')
+        elif age <= 18:
+            print(f'Как жизнь, {name}?')
+        elif age < 100:
+            print(f'Что желаете, {name}?')
+        else:
+            print(f'{name}, вы лжёте - в наше время столько не живут...')
+    except ValueError:
         print('Ошибка, повторите ввод')
-    elif age < 10:
-        print(f'Привет, шкет {name}')
-    elif age <= 18:
-        print(f'Как жизнь, {name}?')
-    elif age < 100:
-        print(f'Что желаете, {name}?')
-    else:
-        print(f'{name}, вы лжёте - в наше время столько не живут...')
-except ValueError:
-    print('Ошибка, повторите ввод')
