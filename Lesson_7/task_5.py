69ca705f01d46b31d142f57e580f3a1a0eaf28b6
san0kq <sanrom94@mail.ru>
2023-01-25 12:37:07 +0300

    function to check number's type in string

---
+++ Lesson_7/task_5.py
@@ -0,0 +1,35 @@
+from typing import NoReturn
+
+
+def what_number(number: str) -> NoReturn:
+    """This is function to check number's type in string"""
+    if number.isdigit():
+        print(f'Вы ввели положительное целое число: {number}')
+    elif '-' in number and '.' in number and number.count('-') == 1 and number.count('.') == 1:
+        new_str = number.replace('-', '')
+        new_str = new_str.replace('.', '')
+        if new_str.isdigit():
+            print(f'Вы ввели отрицательное дробное число: {float(number)}')
+        else:
+            print(f'Вы ввели некорректное число: {number}')
+    elif '-' in number and number.count('-') == 1:
+        new_str = number.replace('-', '')
+        if new_str.isdigit():
+            print(f'Вы ввели отрицательное целое: {int(number)}')
+        else:
+            print(f'Вы ввели некорректное число: {number}')
+    elif '.' in number and number.count('.') == 1:
+        new_str = number.replace('.', '')
+        if new_str.isdigit():
+            print(f'Вы ввели положительное дробное число: {float(number)}')
+        else:
+            print(f'Вы ввели некорректное число: {number}')
+
+    else:
+        print(f'Вы ввели некорректное число: {number}')
+
+
+what_number('-6.7')
+what_number('5')
+what_number('5.4r')
+what_number('-.777')
