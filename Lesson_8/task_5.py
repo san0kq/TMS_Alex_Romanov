5417d6804fdd613deacd2e67f9d7e2c2765d22f1
san0kq <sanrom94@mail.ru>
2023-02-01 19:20:51 +0300

    task 5 with xlsx file

---
+++ Lesson_8/task_5.py
@@ -0,0 +1,34 @@
+"""
+5. Прочитать сохранённый csv-файл и сохранить данные в excel-файл, кроме возраста - столбец с этими данными
+не нужен.
+"""
+
+import openpyxl
+import csv
+from random import randint
+
+with open('task_4.csv') as csv_file:
+    wb = openpyxl.Workbook()                   # открыли новую таблицу
+    wb.create_sheet('Page 1', 0)               # создаём в таблице новую страницу
+    ws = wb['Page 1']                          # переключаемся на страницу для работы с ней
+    col = ['A', 'B', 'C', 'D', 'E', 'F', 'G']  # список нужных столбцов, в которые будет вести запись ниже
+    counter = 2                                # счетчик для движения по столбцу, используется ниже
+    letter = 0                                 # счетчик для движения по строке, то есть по буквам в списке выше
+
+    for index in range(1, len(col)):               # цикл для добавления в первую строку значений Person
+        ws[col[index]+'1'] = f'Person {index}'
+
+    for row in csv.reader(csv_file, delimiter=','):
+        for value in row:
+            if value == 'age':                 # не добавляет название столбца age, как сказано в условии
+                continue
+            elif len(value) != 6 and value.isdigit():  # Если значение является возрастом, меняем его на случайный тел.
+                ws[col[letter]+str(counter)] = f'{randint(100, 999)}-{randint(10, 99)}-{randint(10, 99)}'
+                counter += 1
+            else:
+                ws[col[letter]+str(counter)] = value
+                counter += 1
+        letter += 1                           # добавляем 1 для перехода на следующий столбец, то есть на букву в спике
+        counter = 2                           # возвращаем значение к изначальному для движения по другому столбцу вниз
+
+    wb.save('task_5.xlsx')     # сохраняем документ
