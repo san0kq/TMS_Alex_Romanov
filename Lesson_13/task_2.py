26b8cc068863032f4fa8aa8688bf6ed89ad67380
san0kq <sanrom94@mail.ru>
2023-03-04 12:45:46 +0300

    task_2 is completed

---
+++ Lesson_13/task_2.py
@@ -0,0 +1,73 @@
+"""
+Создать класс Stack, который будет соответствовать структуре данных стек
+(LIFO - last in first out) У класса должны быть методы:
+push(self, element) - добавляет элемент в стек
+pop(self) - выдаёт последний элемент из стека и удаляет его в стеке.
+Если стек пустой, то возвращает None
+is_empty(self) - возвращает True, если стек пустой, False - если нет
+peek(self) - возвращает верхушку стека (последний добавленный элемент),
+в ином случае возвращает None
+
+Использовать этот класс Stack для решения следующей задачи:
+На вход даёт строка из скобок "[](){}"
+Реализовать функцию (обычную, НЕ метод в классе), которая на вход принимает
+строку из скобок И возвращает True, если все скобки закрыты, False - если нет
+"""
+from typing import Any
+
+
+class Stack:
+    def __init__(self) -> None:
+        self._stack = []
+
+    def push(self, element: Any) -> None:
+        self._stack.append(element)
+
+    def pop(self) -> Any:
+        if self._stack:
+            return self._stack.pop()
+        return None
+
+    def is_empty(self) -> bool:
+        return not bool(self._stack)
+
+    def peek(self) -> Any:
+        if self._stack:
+            return self._stack[-1]
+        return None
+
+
+def valid_parentheses(string: str) -> bool:
+    if len(string) % 2 != 0:
+        return False
+    stack = Stack()
+    for par in string:
+        if par in '({[':
+            stack.push(par)
+        else:
+            last_par = stack.pop()
+            if last_par == '(' and par != ')':
+                return False
+            elif last_par == '{' and par != '}':
+                return False
+            elif last_par == '[' and par != ']':
+                return False
+
+    return stack.is_empty()
+
+
+str1 = '{}[]()'
+str2 = '{{}{}'
+str3 = '(((()'
+str4 = '())'
+str5 = ']'
+str6 = '){'
+str7 = '{[()]{}}'
+
+print(valid_parentheses(str1))
+print(valid_parentheses(str2))
+print(valid_parentheses(str3))
+print(valid_parentheses(str4))
+print(valid_parentheses(str5))
+print(valid_parentheses(str6))
+print(valid_parentheses(str7))
