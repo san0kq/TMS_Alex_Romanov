3eab27b6d2f64ac43a2b5e7ec6654707f25a8e06
san0kq <sanrom94@mail.ru>
2023-05-17 20:11:10 +0300

    fixed errors

--- job_board_project/job_board_db.sql
+++ job_board_project/job_board_db.sql
@@ -1,41 +1,52 @@
 CREATE TABLE genders (
-    name VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE languages (
-    name VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE language_levels (
-    name VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE ratings (
-    name VARCHAR(20) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) UNIQUE NOT NULL
 );
 CREATE TABLE positions (
-    name VARCHAR(100) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 CREATE TABLE work_formats (
-    name VARCHAR(50) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 CREATE TABLE statuses (
-    name VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE countries (
-    name VARCHAR(50) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 CREATE TABLE tags (
-    name VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE cities (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    country_name VARCHAR(50) NOT NULL,
-    FOREIGN KEY (country_name) REFERENCES countries(name) ON DELETE CASCADE ON UPDATE CASCADE
+    country_id INTEGER NOT NULL,
+    FOREIGN KEY (country_id) REFERENCES countries(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE contracts (
-    name VARCHAR(40) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40) UNIQUE NOT NULL
 );
 CREATE TABLE levels (
-    name VARCHAR(40) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40) UNIQUE NOT NULL
 );
 CREATE TABLE salaries (
     id SERIAL PRIMARY KEY,
@@ -52,15 +63,17 @@ CREATE TABLE adresses (
     FOREIGN KEY (city_id) REFERENCES cities(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE employees_numbers (
-    size_range VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    size_range VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE sectors (
-    name VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE companies (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) UNIQUE NOT NULL,
-    employees_number_size_range VARCHAR(30),
+    employees_number_id INTEGER,
     founded_in SMALLINT NOT NULL,
     logo TEXT NOT NULL,
     description TEXT NOT NULL,
@@ -70,7 +83,7 @@ CREATE TABLE companies (
     adress_id INTEGER UNIQUE,
     registred_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (employees_number_size_range) REFERENCES employees_numbers(size_range) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (employees_number_id) REFERENCES employees_numbers(id) ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (adress_id) REFERENCES adresses(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 CREATE TABLE employees (
@@ -105,22 +118,22 @@ CREATE TABLE profiles (
     id SERIAL PRIMARY KEY,
     phone_number VARCHAR(20) NOT NULL,
     age SMALLINT CHECK (age BETWEEN 15 AND 121) NOT NULL,
-    gender_name VARCHAR(30),
+    gender_id INTEGER,
     city_id INTEGER,
     image TEXT NOT NULL,
     resume TEXT NOT NULL,
     years_exp SMALLINT CHECK (years_exp BETWEEN -1 AND 120),
-    status_name VARCHAR(30) DEFAULT 'open to work',
-    work_format_name VARCHAR(50),
-    level_name VARCHAR(40),
-    contract_name VARCHAR(40),
+    status_id INTEGER DEFAULT 1,
+    work_format_id INTEGER,
+    level_id INTEGER,
+    contract_id INTEGER,
     salary_id INTEGER,
-    FOREIGN KEY (gender_name) REFERENCES genders(name) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (gender_id) REFERENCES genders(id) ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (city_id) REFERENCES cities(id) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (status_name) REFERENCES statuses(name) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (work_format_name) REFERENCES work_formats(name) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (level_name) REFERENCES levels(name) ON DELETE SET NULL ON UPDATE CASCADE,
-    FOREIGN KEY (contract_name) REFERENCES contracts(name) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (work_format_id) REFERENCES work_formats(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (level_id) REFERENCES levels(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (contract_id) REFERENCES contracts(id) ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (salary_id) REFERENCES salaries(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 CREATE TABLE users (
@@ -137,32 +150,37 @@ CREATE TABLE users (
     FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE profile_languages (
-    profile_id INTEGER,
-    language_name VARCHAR(30),
-    language_lvl_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (profile_id, language_name),
+    id SERIAL PRIMARY KEY,
+    profile_id INTEGER NOT NULL,
+    language_id INTEGER NOT NULL,
+    language_level_id INTEGER NOT NULL,
+    UNIQUE (profile_id, language_id),
     FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (language_name) REFERENCES languages(name) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (language_lvl_name) REFERENCES language_levels(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (language_id) REFERENCES languages(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (language_level_id) REFERENCES language_levels(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
     text VARCHAR(800) NOT NULL,
     user_id INTEGER NOT NULL,
+    company_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+    UNIQUE (user_id, company_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (company_id) REFERENCES companies(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE review_ratings (
-    review_id INTEGER,
-    user_id INTEGER,
-    rating_name VARCHAR(20) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    review_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    rating_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (review_id, user_id),
+    UNIQUE (review_id, user_id),
     FOREIGN KEY (review_id) REFERENCES reviews(id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (rating_name) REFERENCES ratings(name) ON DELETE CASCADE ON UPDATE CASCADE 
+    FOREIGN KEY (rating_id) REFERENCES ratings(id) ON DELETE CASCADE ON UPDATE CASCADE 
 );
 CREATE TABLE social_links (
     id SERIAL PRIMARY KEY,
@@ -179,66 +197,66 @@ CREATE TABLE responses (
     vacancy_id INTEGER NOT NULL,
     text VARCHAR(500) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
-    status_name VARCHAR(30) DEFAULT 'created',
+    status_id INTEGER DEFAULT 4,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (vacancy_id) REFERENCES vacancies(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (status_name) REFERENCES statuses(name) ON DELETE SET NULL ON UPDATE CASCADE
+    FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 CREATE TABLE profiles_tags (
     profile_id INTEGER,
-    tag_name VARCHAR(30),
-    PRIMARY KEY (profile_id, tag_name),
+    tag_id INTEGER,
+    PRIMARY KEY (profile_id, tag_id),
     FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (tag_name) REFERENCES tags(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE employees_positions (
     employee_id INTEGER,
-    position_name VARCHAR(100),
-    PRIMARY KEY (employee_id, position_name),
+    position_id INTEGER,
+    PRIMARY KEY (employee_id, position_id),
     FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (position_name) REFERENCES positions(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (position_id) REFERENCES positions(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE companies_sectors (
     company_id INTEGER,
-    sector_name VARCHAR(30),
-    PRIMARY KEY (company_id, sector_name),
+    sector_id INTEGER,
+    PRIMARY KEY (company_id, sector_id),
     FOREIGN KEY (company_id) REFERENCES companies(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (sector_name) REFERENCES sectors(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (sector_id) REFERENCES sectors(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE vacancies_countries (
     vacancy_id INTEGER,
-    country_name VARCHAR(50),
-    PRIMARY KEY (vacancy_id, country_name),
+    country_id INTEGER,
+    PRIMARY KEY (vacancy_id, country_id),
     FOREIGN KEY (vacancy_id) REFERENCES vacancies(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (country_name) REFERENCES countries(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (country_id) REFERENCES countries(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE vacancies_tags (
     vacancy_id INTEGER,
-    tag_name VARCHAR(30),
-    PRIMARY KEY (vacancy_id, tag_name),
+    tag_id INTEGER,
+    PRIMARY KEY (vacancy_id, tag_id),
     FOREIGN KEY (vacancy_id) REFERENCES vacancies(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (tag_name) REFERENCES tags(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE vacancies_contracts (
     vacancy_id INTEGER,
-    contract_name VARCHAR(40),
-    PRIMARY KEY (vacancy_id, contract_name),
+    contract_id INTEGER,
+    PRIMARY KEY (vacancy_id, contract_id),
     FOREIGN KEY (vacancy_id) REFERENCES vacancies(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (contract_name) REFERENCES contracts(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (contract_id) REFERENCES contracts(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE vacancies_levels (
     vacancy_id INTEGER,
-    level_name VARCHAR(40),
-    PRIMARY KEY (vacancy_id, level_name),
+    level_id INTEGER,
+    PRIMARY KEY (vacancy_id, level_id),
     FOREIGN KEY (vacancy_id) REFERENCES vacancies(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (level_name) REFERENCES levels(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (level_id) REFERENCES levels(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE vacancies_work_formats (
     vacancy_id INTEGER,
-    work_format_name VARCHAR(50),
-    PRIMARY KEY (vacancy_id, work_format_name),
+    work_format_id INTEGER,
+    PRIMARY KEY (vacancy_id, work_format_id),
     FOREIGN KEY (vacancy_id) REFERENCES vacancies(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (work_format_name) REFERENCES work_formats(name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (work_format_id) REFERENCES work_formats(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
